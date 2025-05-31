dcd27d053f5ad593d3a90994664465f1ca0e2c90
desivar <jillian_desire@yahoo.es>
2025-05-31 05:53:25 -0600

    models/student.js

---
+++ models/student.js
@@ -0,0 +1,32 @@
+// models/student.js - This is your Model (M)
+const mongoose = require('mongoose');
+
+const studentSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email',
+        ],
+    },
+    teacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher',
+        required: [true, 'Please assign a teacher to this student'],
+    },
+    dateOfBirth: {
+        type: Date,
+        required: false,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Student', studentSchema);
